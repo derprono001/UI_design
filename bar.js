f87b97766ad7067009edb21fee2d84f291d0ecef
Unknown <vuchuongmb@gmail.com>
2017-12-11 05:55:16 +0200

    huge change

--- bar.js
+++ bar.js
@@ -1,53 +1,17 @@
-$(document).ready(function() {
-$('#slide-nav.navbar-inverse').after($('<div class="inverse" id="navbar-height-col"></div>'));
-$('#slide-nav.navbar-default').after($('<div id="navbar-height-col"></div>'));
+$(document).ready(function () {
+    $("#sidebar").mCustomScrollbar({
+        theme: "minimal"
+    });
 
-var slidewidth = '20%';
-var navbarneg = '-' + slidewidth;
+    $('#dismiss, .overlay').on('click', function () {
+        $('#sidebar').removeClass('active');
+        $('.overlay').fadeOut();
+    });
 
-if ($(window).width() < 767) {
-  $('#navbar-height-col').css("width", slidewidth);
-  $('#navbar-height-col').css("left", navbarneg);
-  $('#slide-nav #slidemenu').css("width", slidewidth);
-  $('#slide-nav #slidemenu').css("left", navbarneg);
-}
-
-$("#slide-nav").on("click", '.navbar-toggle', function(e) {
-
-  // slider is active
-  var selected = $(this).hasClass('slide-active');
-
-  // set slidemenu width
-  $('#slidemenu').stop().animate({
-    left: selected ? navbarneg : '0px'
-  });
-
-  // set navbar width
-  $('#navbar-height-col').stop().animate({
-    left: selected ? navbarneg : '0px'
-  });
-
-  // set content let
-  $('#page-content').stop().animate({
-    left: selected ? '0px' : slidewidth
-  });
-
-  // set navbar left
-  $('.navbar-header').stop().animate({
-    left: selected ? '0px' : slidewidth
-  });
-
-  $(this).toggleClass('slide-active', !selected);
-  $('#slidemenu').toggleClass('slide-active');
-
-  $('#page-content, .navbar, body, .navbar-header').toggleClass('slide-active');
-});
-
-var selected = '#slidemenu, #page-content, body, .navbar, .navbar-header';
-
-$(window).on("resize", function() {
-  if ($(window).width() > 767 && $('.navbar-toggle').is(':hidden')) {
-    $(selected).removeClass('slide-active');
-  }
-});
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').addClass('active');
+        $('.overlay').fadeIn();
+        $('.collapse.in').toggleClass('in');
+        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+    });
 });
